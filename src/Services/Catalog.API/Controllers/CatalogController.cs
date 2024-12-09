2fae5c4357d91a10890964a5314df82c6617ff1c
Sumaiya jouty <farhanajouty87@gmail.com>
2024-12-09 17:51:07 +0600

    changes

--- src/Services/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -3,6 +3,7 @@ using Catalog.API.Models;
 using CoreApiResponse;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using System.Net;
 
 namespace Catalog.API.Controllers
@@ -23,8 +24,42 @@ namespace Catalog.API.Controllers
         [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
         public IActionResult GetProducts()
         {
-            var product = _productManager.GetAll();
-            return CustomResult(product,HttpStatusCode.Accepted);
+
+            try
+            {
+                var product = _productManager.GetAll();
+                return CustomResult(product, HttpStatusCode.Accepted);
+            }
+            catch (Exception ex)
+            {
+
+                return CustomResult(ex.Message,HttpStatusCode.BadRequest);
+            }
+          
+
+
+        }
+
+
+        [HttpGet]
+        [Route("api/Catalog/AddProduct")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public IActionResult AddProduct([FromBody]Product product)
+        {
+
+            try
+            {
+                product.Id=ObjectId.GenerateNewId().ToString();
+                var isSaved = _productManager.Add(product);
+
+                return CustomResult(product, HttpStatusCode.Accepted);
+            }
+            catch (Exception ex)
+            {
+
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+
 
 
         }
